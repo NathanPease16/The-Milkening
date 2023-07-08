45758a9ea8a38464bdfe00250bb650351de28a16
knowndisc <sun.pauly@gmail.com>
2023-07-08 11:29:30 -0500

    Post Processing To Heat Zones
    Very cool stuff

--- The Milkening/Assets/Scripts/Health/HeatZone.cs
+++ The Milkening/Assets/Scripts/Health/HeatZone.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 public class HeatZone : MonoBehaviour
 {
     [Header("Attributes")]
@@ -8,14 +9,23 @@ public class HeatZone : MonoBehaviour
     [Header("References")]
     private MilkLevel milk;
 
+    Volume v;
+    Bloom b;
+
     private void Awake()
     {
         milk = GameObject.FindGameObjectWithTag("Player").GetComponent<MilkLevel>();
+        v = GetComponent<Volume>();
+        v.profile.TryGet(out b);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-            milk.CurrentTemperature = temperature;
+            {
+                milk.CurrentTemperature = temperature;
+                b.intensity.value = Mathf.Clamp(Mathf.FloorToInt(20f * (temperature/100)), 0, 20);
+            }
+
     }
 }
